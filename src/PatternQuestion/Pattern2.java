46fa35de0891b7087200cade88e1316f2bf3fcfe
Hrishik Gupta <hrishikgupta2015@gmail.com>
2025-02-22 23:55:48 +0530

    Pattern Question

--- src/PatternQuestion/Pattern2.java
+++ src/PatternQuestion/Pattern2.java
@@ -1,4 +1,22 @@
 package PatternQuestion;
 
+//input n = 5
+//        *
+//        * *
+//        * * *
+//        * * * *
+//        * * * * *
+
 public class Pattern2 {
+    public static void main(String[] args) {
+        pattern(5);
+    }
+    static void pattern(int n){
+        for(int row = 0;row<n;row++){
+            for(int col=0;col<=row;col++){
+                System.out.print("*"+" ");
+            }
+            System.out.println();
+        }
+    }
 }
--- src/PatternQuestion/Pattern3.java
+++ src/PatternQuestion/Pattern3.java
@@ -1,4 +1,21 @@
 package PatternQuestion;
+//Input n = 5
+//        * * * * *
+//        * * * *
+//        * * *
+//        * *
+//        *
 
 public class Pattern3 {
+    public static void main(String[] args) {
+        pattern(5);
+    }
+    static void pattern(int n){
+        for(int row=1;row<=n;row++){
+            for (int col = n;col>=row;col--){
+                System.out.print("*"+" ");
+            }
+            System.out.println();
+        }
+    }
 }
