837b66247fef116ee9776dfdd4d749fa00f8b6b8
Hrishik Gupta <hrishikgupta2015@gmail.com>
2025-04-08 12:56:41 +0530

    Fibonnaci Using Recursion with direct formulla

--- src/Recursion/Fibonnaci.java
+++ src/Recursion/Fibonnaci.java
@@ -2,12 +2,24 @@ package Recursion;
 
 public class Fibonnaci {
     public static void main(String[] args) {
-        System.out.println(fibo(6));
+        for(int i=0;i<=50;i++){
+            //System.out.print(fibo(i)+" ");
+            System.out.print(fiboUsingFormulla(i)+ " ");
+            System.out.println(fiboUsingFormulla2(i));
+        }
     }
     static int fibo(int n){
         if(n<2){
             return n;
         }
         return fibo(n-1)+fibo(n-2);
+    }
+
+    static int fiboUsingFormulla(int n){
+        return (int)((Math.pow(((1+Math.sqrt(5))/2),n) - Math.pow(((1-Math.sqrt(5))/2),n))/Math.sqrt(5));
+    }
+    //Removing the less dominating term from formulla
+    static int fiboUsingFormulla2(int n){
+        return (int) (Math.pow(((1+Math.sqrt(5))/2),n)/Math.sqrt(5));
     }
 }
