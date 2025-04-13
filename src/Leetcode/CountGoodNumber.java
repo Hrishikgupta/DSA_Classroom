b8fdfda750f379776b1e741a43cd61a00afeb111
Hrishik Gupta <hrishikgupta2015@gmail.com>
2025-04-13 23:01:31 +0530

    leetcode-1922

---
+++ src/Leetcode/CountGoodNumber.java
@@ -0,0 +1,11 @@
+package Leetcode;
+//https://leetcode.com/problems/count-good-numbers/?envType=daily-question&envId=2025-04-13
+//leetcode-1922
+public class CountGoodNumber {
+    public static void main(String[] args) {
+
+    }
+    public static int countGoodNumbers(long n) {
+
+    }
+}
