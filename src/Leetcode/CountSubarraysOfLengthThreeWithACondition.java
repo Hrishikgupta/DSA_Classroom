43fbafd209049aa486479ffecd21600c4885a430
Hrishik Gupta <hrishikgupta2015@gmail.com>
2025-04-28 01:47:42 +0530

    leetcode-3392

--- src/Leetcode/CountSubarraysOfLengthThreeWithACondition.java
+++ src/Leetcode/CountSubarraysOfLengthThreeWithACondition.java
@@ -3,10 +3,17 @@ package Leetcode;
 //leetcode-3392
 public class CountSubarraysOfLengthThreeWithACondition {
     public static void main(String[] args) {
-        int[] nums = {1,2,1,4,1};
-
+        //            0,1,2,3,4,5
+        int[] nums = {-1,-4,-1,4};
+        System.out.println(countSubarrays(nums));
     }
     public static int countSubarrays(int[] nums) {
-
+        int count = 0;
+        for(int i=0;i<nums.length-2;i++){
+            if(2*(nums[i]+nums[i+2])==nums[i+1]){
+                count++;
+            }
+        }
+        return count;
     }
 }
