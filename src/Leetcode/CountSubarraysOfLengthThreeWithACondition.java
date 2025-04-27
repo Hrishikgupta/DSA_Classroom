05cc4733c12a3b162b453a500a0e5e8b52ffba4f
Hrishik Gupta <hrishikgupta2015@gmail.com>
2025-04-27 22:22:24 +0530

    leetcode-3392

---
+++ src/Leetcode/CountSubarraysOfLengthThreeWithACondition.java
@@ -0,0 +1,12 @@
+package Leetcode;
+//https://leetcode.com/problems/count-subarrays-of-length-three-with-a-condition/?envType=daily-question&envId=2025-04-27
+//leetcode-3392
+public class CountSubarraysOfLengthThreeWithACondition {
+    public static void main(String[] args) {
+        int[] nums = {1,2,1,4,1};
+
+    }
+    public static int countSubarrays(int[] nums) {
+
+    }
+}
