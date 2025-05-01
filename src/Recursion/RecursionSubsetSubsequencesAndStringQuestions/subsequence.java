ed013347d4bb3a493d349cd4e831f8639ce8cd42
Hrishik Gupta <hrishikgupta2015@gmail.com>
2025-05-01 23:13:11 +0530

    Recursion Question with String

---
+++ src/Recursion/RecursionSubsetSubsequencesAndStringQuestions/subsequence.java
@@ -0,0 +1,17 @@
+package Recursion.RecursionSubsetSubsequencesAndStringQuestions;
+
+public class subsequence {
+    public static void main(String[] args) {
+        subSequence("","abc");
+    }
+    static void subSequence(String p, String up){
+        if(up.isEmpty()){
+            System.out.println(p);
+            return;
+        }
+
+        char ch = up.charAt(0);
+        subSequence(p+ch,up.substring(1));
+        subSequence(p,up.substring(1));
+    }
+}
