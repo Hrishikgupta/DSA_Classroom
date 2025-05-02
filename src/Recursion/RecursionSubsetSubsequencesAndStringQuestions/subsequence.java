2438a4db5dd90a2fc04793c246615e5642237a16
Hrishik Gupta <hrishikgupta2015@gmail.com>
2025-05-02 22:13:02 +0530

    Recursion Question with String

--- src/Recursion/RecursionSubsetSubsequencesAndStringQuestions/subsequence.java
+++ src/Recursion/RecursionSubsetSubsequencesAndStringQuestions/subsequence.java
@@ -1,9 +1,13 @@
 package Recursion.RecursionSubsetSubsequencesAndStringQuestions;
 
+import java.util.ArrayList;
+
 public class subsequence {
     public static void main(String[] args) {
         subSequence("","abc");
+        System.out.println(subSequenceRet("","abc"));
     }
+
     static void subSequence(String p, String up){
         if(up.isEmpty()){
             System.out.println(p);
@@ -13,5 +17,19 @@ public class subsequence {
         char ch = up.charAt(0);
         subSequence(p+ch,up.substring(1));
         subSequence(p,up.substring(1));
+    }
+
+    static ArrayList<String> subSequenceRet(String p, String up){
+        if(up.isEmpty()){
+            ArrayList<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+
+        char ch = up.charAt(0);
+        ArrayList<String>  left =subSequenceRet(p+ch,up.substring(1));
+        ArrayList<String> right =subSequenceRet(p,up.substring(1));
+        left.addAll(right);
+        return left;
     }
 }
