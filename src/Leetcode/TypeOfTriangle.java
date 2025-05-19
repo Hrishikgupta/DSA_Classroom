4bdc35782fe72c2fc9e17f659f26eed93945168a
Hrishik Gupta <hrishikgupta2015@gmail.com>
2025-05-19 23:57:27 +0530

    leetcode-3024

---
+++ src/Leetcode/TypeOfTriangle.java
@@ -0,0 +1,8 @@
+package Leetcode;
+//https://leetcode.com/problems/type-of-triangle/description/?envType=daily-question&envId=2025-05-19
+//leetcode-3024
+public class TypeOfTriangle {
+    public static void main(String[] args) {
+
+    }
+}
