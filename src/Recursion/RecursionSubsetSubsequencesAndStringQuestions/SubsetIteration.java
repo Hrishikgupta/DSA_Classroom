9d0baa00c4ad50bb10e2bdb278ed6c302a4ee0c4
Hrishik Gupta <hrishikgupta2015@gmail.com>
2025-06-09 23:59:06 +0530

    Subset Iteration

--- src/Recursion/RecursionSubsetSubsequencesAndStringQuestions/SubsetIteration.java
+++ src/Recursion/RecursionSubsetSubsequencesAndStringQuestions/SubsetIteration.java
@@ -1,4 +1,45 @@
 package Recursion.RecursionSubsetSubsequencesAndStringQuestions;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class SubsetIteration {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3};
+        List<List<Integer>> ans = subset(arr);
+        for(List<Integer> list: ans){
+            System.out.println(list);
+        }
+    }
+    static List<List<Integer>> subset(int[] arr){
+        List<List<Integer>> outer = new ArrayList<>();
+        outer.add(new ArrayList<>());
+
+        for(int num: arr){
+            int n = outer.size();
+            for(int i=0; i<n; i++){
+                List<Integer> internal = new ArrayList<>(outer.get(i));
+                internal.add(num);
+                outer.add(internal);
+            }
+        }
+        return outer;
+    }
+
+    static List<List<Integer>> subsetDuplicate(int[] arr){
+        Arrays.sort(arr);
+        List<List<Integer>> outer = new ArrayList<>();
+        outer.add(new ArrayList<>());
+
+        for(int num: arr){
+            int n = outer.size();
+            for(int i=0; i<n; i++){
+                List<Integer> internal = new ArrayList<>(outer.get(i));
+                internal.add(num);
+                outer.add(internal);
+            }
+        }
+        return outer;
+    }
 }
